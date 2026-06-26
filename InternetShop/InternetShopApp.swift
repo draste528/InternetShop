df9574954ed832dd74b9b2b98a543de99fdc08a8
myOS <myos@10.0.2.15>
2026-06-26 20:51:56 +0300

    Initial Xcode project setup

---
+++ InternetShop/InternetShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternetShopApp.swift
+//  InternetShop
+//
+//  Created by myOS on 26.06.26.
+//
+
+import SwiftUI
+
+@main
+struct InternetShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
