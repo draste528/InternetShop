8373a105de7291dc2e8096257f3d743a0edc8bbc
draste528 <draste528@gmail.com>
2026-07-06 00:04:29 +0300

    Added components for HomePage : StartShopButton , BrandSection. Updated : ContentView.swift

--- InternetShop/Components/BrandSection.swift
+++ InternetShop/Components/BrandSection.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct BrandSection: View {
+
+    var body: some View {
+
+        VStack(spacing: 12) {
+
+            Image("LOGO")
+
+            Text("MINIMALIST")
+                .font(.system(size: 20, weight: .bold))
+
+            Text("furniture store")
+                .font(.system(size: 12))
+        }
+    }
+}
--- InternetShop/Components/StartShopButton.swift
+++ InternetShop/Components/StartShopButton.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct StartShopButton: View {
+
+    var body: some View {
+
+        Button {
+
+        } label: {
+
+            Text("START SHOP")
+                .font(.headline)
+                .foregroundStyle(Color("White"))
+                .frame(maxWidth: .infinity)
+                .frame(height: 56)
+                .background(Color("Primary"))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+    }
+}
